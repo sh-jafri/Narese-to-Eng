fd9a3985f2da06548450546d5cd789d2876991ef
Jonathan Cherry <tuo97200@temple.edu>
2025-04-18 20:54:11 -0400

    User Input
    - Create baseline user input acceptence
    - Establish Necessary functionality for tool

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-
+import os
 
 def main():
-    print("Hello World!")
-
+    print(translate("Nal-1.txt"))
 
 
 def translate(file):
-    f = open(file, 'r')
+    path = os.path.join("Nal Examples", file)
+    f = open(path, 'r')
     content = f.read()
-    print(content)
+    return(content)
+
+if __name__ == "__main__":
+    main()
+
+def translateInput(task):
+    return 0
+
+
+def translateFile(file):
+    return 0
+
+def copulas(copula):
+    return 0
+
+def getSentenceType(sentence):
+    return 
+
+
+##Allow for users to input their own NAL file.
+def readNalFile():
+    filepath = input("Enter the path to your NAL file: ").strip()
+    try:
+        fileContent = translate(filepath)
+        print(fileContent)
+    except FileNotFoundError:
+        print("File not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
